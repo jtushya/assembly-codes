29db2005e725522e855dd9d471bbf20e99baa9ea
jtushya <techtushya@gmail.com>
2025-04-16 20:05:38 +0530

    Refactor factorial calculation in assembly code for clarity and consistency

--- factorial.asm
+++ factorial.asm
@@ -1,34 +1,30 @@
 .model small
 .data
 
-fact dw ?
-msg db "enter a number from 1 to 8: $" ; use to terminate the string
+fact dw ?                     ; Variable to store the factorial result
+msg db "enter a number from 1 to 8: $" ; Message to prompt the user for input
 
 .code
 .startup  
 
-; this line will be displayed on the screen, in its ascii value
-;so we do masking to get the actual value of the number
+mov dx, offset msg            ; Load the address of the message into DX
+mov ah, 09h                   ; DOS interrupt function to display a string
+int 21h                       ; Call DOS interrupt to display the message
 
-mov dx, offset msg ; this is used to print the msg
-mov ah,09h ; use for function 09h
-int 21h  ; using dos interrupt 21h
-                                     
-;now take input
-mov ah, 01h
-int 21h              
-                                     
-and al,0fh; used to get the first nibble, or ah=00h
-mov ch,00
-mov cl,al; set up a counter al, al-1,....1
-mov ax,0001h        
+mov ah, 1h                    ; DOS interrupt function to read a single character
+int 21h                       ; Call DOS interrupt to read the input character
 
-L1:
-  mul cl
-  loop l1
-  
- mov fact,ax  
- 
- .exit
- end
-  
+and ax, 0fh                   ; Convert ASCII input to its numeric value (0-9)
+
+mov cx, ax                    ; Store the input number in CX (loop counter)
+mov ax, 01h                   ; Initialize AX to 1 (used for multiplication)
+
+l1:                           ; Label for the loop
+    mul cx                    ; Multiply AX by CX (AX = AX * CX)
+    loop l1                   ; Decrement CX and repeat the loop if CX != 0
+                
+mov fact, ax                  ; Store the result (factorial) in the variable 'fact'
+
+end                           ; End of the program
+.exit                         ; Exit the program
+
--- factorial_recursive.asm
+++ factorial_recursive.asm
@@ -2,32 +2,32 @@
 .stack 100h
 .data      
 
-N dw 5
-factorial dw ?
+lN dw 5                      ; Variable to store the input number for factorial
+factorial dw ?              ; Variable to store the calculated factorial result
 
 .code
 ;...function defined   
-fact proc:
-    cmp ax,1
-    jbe baseCase
-    push ax
-    dec ax
-    call fact
-    pop bx
-    mul bx
-    ret
+fact proc:                  ; Start of the factorial procedure
+    cmp ax,1               ; Compare AX with 1 to check if it is the base case
+    jbe baseCase           ; If AX <= 1, jump to the base case
+    push ax                ; Push the current value of AX onto the stack
+    dec ax                 ; Decrement AX by 1
+    call fact              ; Recursive call to the factorial procedure
+    pop bx                 ; Pop the previous value of AX from the stack into BX
+    mul bx                 ; Multiply AX by BX (AX = AX * BX)
+    ret                    ; Return from the procedure
        
-   baseCase:
-     mov ax,1
-     ret
-     fact endp
+   baseCase:               ; Base case for the recursion
+     mov ax,1              ; Set AX to 1 (factorial of 1 is 1)
+     ret                   ; Return from the procedure
+     fact endp             ; End of the factorial procedure
 
 .startup
 ;...write the program
-mov ax,1000h
-mov ds,ax
-mov ax,N
-call fact
-mov factorial,ax
-.exit
+mov ax,1000h               ; Initialize AX with the segment address for data
+mov ds,ax                  ; Set DS to point to the data segment
+mov ax,N                   ; Load the input number (N) into AX
+call fact                  ; Call the factorial procedure
+mov factorial,ax           ; Store the result of the factorial in the variable 'factorial'
+.exit                      ; Exit the program
 end
